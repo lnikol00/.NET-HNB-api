eb29c05ea5e541aaf149ab9a225e52149cfde45c
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-04-25 15:45:10 +0200

    Kreiranje main controllera

---
+++ KingAcadamey/KingAcadamey/Controllers/CurrencyController.cs
@@ -0,0 +1,25 @@
+﻿using KingAcadamey.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KingAcadamey.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly IHnbService _hnbService;
+
+        public CurrencyController(IHnbService hnbService)
+        {
+            _hnbService = hnbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetCurrencies([FromQuery] string fromDate, [FromQuery] string toDate)
+        {
+            var list = _hnbService.GetHnbValues(fromDate, toDate);
+            return Ok(list);
+        }
+    }
+}
